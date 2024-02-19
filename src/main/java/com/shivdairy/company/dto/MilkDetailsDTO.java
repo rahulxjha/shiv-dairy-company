b1f49ef13b257f7f6f6979f08a483c41b7c4cccc
Ankur Verma <ankur.verma@mulberri.io>
2024-02-19 12:27:19 +0530

    mapped both the dtos

--- src/main/java/com/shivdairy/company/dto/MilkDetailsDTO.java
+++ src/main/java/com/shivdairy/company/dto/MilkDetailsDTO.java
@@ -2,6 +2,7 @@ package com.shivdairy.company.dto;
 
 import jakarta.persistence.*;
 import lombok.Data;
+import lombok.Getter;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -14,22 +15,34 @@ public class MilkDetailsDTO {
     @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "milk_sequence")
     @SequenceGenerator(name = "milk_sequence", sequenceName = "milk_sequence", initialValue = 1011, allocationSize = 1)
     private Long id;
+
     @Column(name = "milk_type", nullable = false)
     private MilkType milkType;
+
     @Column(name = "milk_amount", nullable = false)
     private Double milkAmount;
+
     @Column(name = "milk_fat", nullable = false)
     private Double fat;
+
     @Column(name = "milk_clr", nullable = false)
     private Double clr;
+
     @Column(name = "milk_snf")
     private Double snf;
+
     @Column(name = "milk_rate", nullable = false)
     private Double rate;
+
     @Column(name = "milk_powder")
     private Double powder;
+
     @Column(name = "supplier_milk_payment")
     private Double milkPayment;
+
+    @ManyToOne
+    @JoinColumn(name = "supplier_id")
+    private SupplierDTO supplier;
 
     public Map<String, String> toMap() {
         Map<String, String> milkMap = new HashMap<>();
--- src/main/java/com/shivdairy/company/dto/SupplierDTO.java
+++ src/main/java/com/shivdairy/company/dto/SupplierDTO.java
@@ -11,6 +11,7 @@ import org.springframework.data.annotation.CreatedDate;
 
 import java.time.LocalDateTime;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @Data
@@ -21,34 +22,46 @@ public class SupplierDTO {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(nullable = false, unique = true)
     private Integer id;
+
     @Column(name = "supplier_name")
     @NotBlank(message = "Invalid or missing Supplier Name")
     @Length(max = 50)
     private String name;
+
     @Column(name = "phone_num", nullable = false, length = 10)
     @NotNull(message = "Invalid or missing Phone Number")
     @Pattern(regexp = "^(\\+91|0)?[6789]\\d{9}$", message = "Invalid or missing Phone Number")
     private String phoneNum;
+
     @Column(name = "supplier_email", unique = true)
     @Length(max = 256)
     @Email(message = "Invalid or missing Contact Email.", regexp = "^[A-Za-z0-9+_.-]+@(.+)$")
     @NotBlank(message = "Invalid or missing Contact Email.")
     private String email;
+
     @Column(name = "address", nullable = false)
     @NotBlank(message = "Invalid or missing Supplier Address")
     @Length(max = 150)
     private String address;
+
     @CreatedDate
     @Column(name = "created_dt", nullable = false)
     private LocalDateTime createdDt;
+
     @Column(name = "updated_dt")
     private LocalDateTime updatedDt;
+
     @Column(name = "supplier_payment_status")
     private PaymentStatus paymentStatus = PaymentStatus.NOT_DONE;
+
     @Column
     private Boolean isDeleted = Boolean.FALSE;
+
     @Column(name = "deleted_dt")
     private LocalDateTime deletedDt;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "supplier")
+    private List<MilkDetailsDTO> milkDetailsDTO;
 
     public Map<String, String> toMap() {
         Map<String, String> supplierMap = new HashMap<>();

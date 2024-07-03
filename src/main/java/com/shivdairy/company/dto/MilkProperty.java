6298eda446468530264df80b03a9c41dfddd76cc
Rahul Jha <rjrahuljha02@gmail.com>
2024-07-03 20:53:22 +0530

    created super class for better handling

---
+++ src/main/java/com/shivdairy/company/dto/MilkProperty.java
@@ -0,0 +1,9 @@
+package com.shivdairy.company.dto;
+
+public interface MilkProperty {
+    String getName();
+    Double getMilkWeight();
+    Double getFat();
+    Double getClr();
+    Double getMilkRate();
+}

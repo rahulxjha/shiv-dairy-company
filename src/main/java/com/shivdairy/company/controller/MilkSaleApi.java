4eb3613a89ef066e24a6975e1e6cb0b0a686763e
Rahul Jha <rjrahuljha02@gmail.com>
2024-07-15 15:37:58 +0530

    refactored milk sale api

--- src/main/java/com/shivdairy/company/controller/MilkSaleApi.java
+++ src/main/java/com/shivdairy/company/controller/MilkSaleApi.java
@@ -4,24 +4,22 @@ import com.shivdairy.company.constant.MilkConstant;
 import com.shivdairy.company.dto.BaseResponseDTO;
 import com.shivdairy.company.dto.MilkSaleRequestDTO;
 import com.shivdairy.company.model.MilkSaleDetails;
+import com.shivdairy.company.service.MilkSaleService;
 import com.shivdairy.company.service.MilkService;
 import jakarta.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @Slf4j
 @RestController
 @RequestMapping(value = "public/api/v1")
 public class MilkSaleApi {
     @Autowired
-    private MilkService milkService;
+    private MilkSaleService milkService;
 
-    @GetMapping("/calculateBuyerMilkProperty")
+    @PostMapping("/calculateBuyerMilkProperty")
     public ResponseEntity<BaseResponseDTO<MilkSaleDetails>> calculateMilkProperty(@Valid @RequestBody MilkSaleRequestDTO milkSaleRequestDTO){
         log.info("Requesting for api/v1/calculateMilkProperty with RequestBody: {}", milkSaleRequestDTO);
         MilkSaleDetails milkSaleDetails = milkService.saveMilkSaleDetails(milkSaleRequestDTO);

801cb390daf654eac9a5e56692eee721d42dcdf3
Rahul Jha <rjrahuljha02@gmail.com>
2024-07-17 20:45:37 +0530

    added two methods in MilkSaleApi and its required changes

--- src/main/java/com/shivdairy/company/repository/MilkSaleRepository.java
+++ src/main/java/com/shivdairy/company/repository/MilkSaleRepository.java
@@ -2,6 +2,14 @@ package com.shivdairy.company.repository;
 
 import com.shivdairy.company.model.MilkSaleDetails;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.time.LocalDate;
+import java.util.List;
 
 public interface MilkSaleRepository extends JpaRepository<MilkSaleDetails, Long> {
+    @Query("SELECT m FROM MilkSaleDetails m WHERE m.buyerName LIKE %:buyerName% AND m.date BETWEEN :startDate AND :endDate")
+    List<MilkSaleDetails> getMilkSaleDetails(@Param("buyerName") String buyerName, @Param("startDate") LocalDate startDate,
+                        @Param("endDate") LocalDate endDate);
 }

f44013d2d78a474cfcf7403bd223c6d49516dd1c
Rahul Jha <rjrahuljha02@gmail.com>
2024-07-19 19:01:03 +0530

    added generatePdfForMilkDetails method

--- src/main/java/com/shivdairy/company/service/PdfService.java
+++ src/main/java/com/shivdairy/company/service/PdfService.java
@@ -4,4 +4,5 @@ import java.io.ByteArrayInputStream;
 
 public interface PdfService {
     ByteArrayInputStream generatePdfForMilkSaleDetails();
+    ByteArrayInputStream generatePdfForMilkDetails();
 }
--- src/main/java/com/shivdairy/company/service/impl/PdfServiceImpl.java
+++ src/main/java/com/shivdairy/company/service/impl/PdfServiceImpl.java
@@ -6,8 +6,10 @@ import com.itextpdf.layout.Document;
 import com.itextpdf.layout.element.Cell;
 import com.itextpdf.layout.element.Paragraph;
 import com.itextpdf.layout.element.Table;
+import com.shivdairy.company.model.MilkDetails;
 import com.shivdairy.company.model.MilkSaleDetails;
 import com.shivdairy.company.service.MilkSaleService;
+import com.shivdairy.company.service.MilkService;
 import com.shivdairy.company.service.PdfService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -21,6 +23,8 @@ public class PdfServiceImpl implements PdfService {
 
     @Autowired
     private MilkSaleService milkSaleService;
+    @Autowired
+    private MilkService milkService;
 
     @Override
     public ByteArrayInputStream generatePdfForMilkSaleDetails() {
@@ -30,7 +34,7 @@ public class PdfServiceImpl implements PdfService {
         PdfDocument pdfDocument = new PdfDocument(writer);
         Document document = new Document(pdfDocument);
 
-        document.add(new Paragraph("Shiv Dairy"));
+        document.add(new Paragraph("Shiv Dairy Milk Sale Details"));
 
         int columnSize = 20;
         Table table = new Table(columnSize);
@@ -78,6 +82,60 @@ public class PdfServiceImpl implements PdfService {
             table.addCell(new Cell().add(new Paragraph(saleDetail.getMilkWeight().toString())));
             table.addCell(new Cell().add(new Paragraph(saleDetail.getFat().toString())));
             table.addCell(new Cell().add(new Paragraph(saleDetail.getClr().toString())));
+        }
+
+        document.add(table);
+        document.close();
+
+        return new ByteArrayInputStream(baos.toByteArray());
+    }
+
+    public ByteArrayInputStream generatePdfForMilkDetails() {
+        List<MilkDetails> milkDetails = milkService.getAllMilkDetails();
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        PdfWriter writer = new PdfWriter(baos);
+        PdfDocument pdfDocument = new PdfDocument(writer);
+        Document document = new Document(pdfDocument);
+
+        document.add(new Paragraph("Shiv Dairy Milk Collection Details"));
+
+        int columnSize = 16;
+        Table table = new Table(columnSize);
+        table.setFontSize(8);
+        table.addHeaderCell(new Cell().add(new Paragraph("ID")));
+        table.addHeaderCell(new Cell().add(new Paragraph("Supplier Name")));
+        table.addHeaderCell(new Cell().add(new Paragraph("Milk Weight")));
+        table.addHeaderCell(new Cell().add(new Paragraph("Fat")));
+        table.addHeaderCell(new Cell().add(new Paragraph("CLR")));
+        table.addHeaderCell(new Cell().add(new Paragraph("Date")));
+        table.addHeaderCell(new Cell().add(new Paragraph("Fat Amount")));
+        table.addHeaderCell(new Cell().add(new Paragraph("Fat Weight")));
+        table.addHeaderCell(new Cell().add(new Paragraph("Fat Rate")));
+        table.addHeaderCell(new Cell().add(new Paragraph("SNF Weight")));
+        table.addHeaderCell(new Cell().add(new Paragraph("SNF Amount")));
+        table.addHeaderCell(new Cell().add(new Paragraph("SNF Percent")));
+        table.addHeaderCell(new Cell().add(new Paragraph("SNF Rate")));
+        table.addHeaderCell(new Cell().add(new Paragraph("Milk Rate")));
+        table.addHeaderCell(new Cell().add(new Paragraph("Milk Payment")));
+        table.addHeaderCell(new Cell().add(new Paragraph("Milk Type")));
+
+        for (MilkDetails milkDetail : milkDetails) {
+            table.addCell(new Cell().add(new Paragraph(milkDetail.getId().toString())));
+            table.addCell(new Cell().add(new Paragraph(milkDetail.getName())));
+            table.addCell(new Cell().add(new Paragraph(milkDetail.getMilkWeight().toString())));
+            table.addCell(new Cell().add(new Paragraph(milkDetail.getFat().toString())));
+            table.addCell(new Cell().add(new Paragraph(milkDetail.getClr().toString())));
+            table.addCell(new Cell().add(new Paragraph(milkDetail.getDate().toString())));
+            table.addCell(new Cell().add(new Paragraph(milkDetail.getFatAmount().toString())));
+            table.addCell(new Cell().add(new Paragraph(milkDetail.getFatWeight().toString())));
+            table.addCell(new Cell().add(new Paragraph(milkDetail.getFatRate().toString())));
+            table.addCell(new Cell().add(new Paragraph(milkDetail.getSnfWeight().toString())));
+            table.addCell(new Cell().add(new Paragraph(milkDetail.getSnfAmount().toString())));
+            table.addCell(new Cell().add(new Paragraph(milkDetail.getSnfPercent().toString())));
+            table.addCell(new Cell().add(new Paragraph(milkDetail.getSnfRate().toString())));
+            table.addCell(new Cell().add(new Paragraph(milkDetail.getMilkRate().toString())));
+            table.addCell(new Cell().add(new Paragraph(milkDetail.getMilkPayment().toString())));
+            table.addCell(new Cell().add(new Paragraph(milkDetail.getMilkType().toString())));
         }
 
         document.add(table);

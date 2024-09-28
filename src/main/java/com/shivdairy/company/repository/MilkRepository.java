2b952f58864b60ccf9faf4a05f7ed4122a208be8
Rahul Jha <rjrahuljha02@gmail.com>
2024-09-29 00:33:18 +0530

    added getMilkDetails by name

--- src/main/java/com/shivdairy/company/repository/MilkRepository.java
+++ src/main/java/com/shivdairy/company/repository/MilkRepository.java
@@ -1,6 +1,7 @@
 package com.shivdairy.company.repository;
 
 import com.shivdairy.company.model.MilkDetails;
+import com.shivdairy.company.model.MilkSaleDetails;
 import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -15,5 +16,11 @@ import java.util.List;
 public interface MilkRepository extends JpaRepository<MilkDetails, String> {
     @Query("SELECT m FROM MilkDetails m WHERE m.name LIKE %:name% AND m.date BETWEEN :startDate AND :endDate")
     List<MilkDetails> getMilkPayment(@Param("name") String name, @Param("startDate") LocalDate startDate, @Param("endDate") LocalDate endDate);
+
+    @Query("SELECT m FROM MilkDetails m WHERE m.name LIKE %:supplierName% AND m.date BETWEEN :effectiveDate " +
+            "AND " +
+            ":endDate")
+    List<MilkDetails> getMilkDetails(@Param("supplierName") String supplierName, @Param("effectiveDate") LocalDate effectiveDate,
+                                         @Param("endDate") LocalDate endDate);
 
 }
